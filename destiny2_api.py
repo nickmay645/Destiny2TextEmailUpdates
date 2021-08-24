cd54e175f68a5ef861cc00850b08ae8d6c991ecf
Nick <nickmay645@gmail.com>
2021-08-24 00:46:03 -0400

    added in auto running in background

--- destiny2_api.py
+++ destiny2_api.py
@@ -1,34 +1,15 @@
+import time
+
 from selenium import webdriver
-import sms
 import smtplib
 from email.message import EmailMessage
+from datetime import datetime
 
 vendor_scan_list = ["ADA-1", "BANSHEE-44"]
 info_list = ["MATERIAL EXCHANGE"]
 
 
-def email_alert(subject, body, to):
-    # 160 char max
-
-    msg = EmailMessage()
-    msg.set_content(body)
-    msg['subject'] = subject
-    msg['to'] = to
-
-    user = "destiny2smsupdates@gmail.com"
-    password = ""
-    msg['from'] = user
-
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(user, password)
-    server.send_message(msg)
-
-    server.quit()
-
-
-if __name__ == '__main__':
-
+def pull_data():
     url = 'https://www.todayindestiny.com/vendors'
     driver = webdriver.Chrome('chromedriver.exe')
     driver.get(url)
@@ -55,3 +36,46 @@ if __name__ == '__main__':
                             'textContent')
 
             email_alert("Mods", message, "3307013905@messaging.sprintpcs.com")
+
+
+def email_alert(subject, body, to):
+    # 160 char max
+
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+
+    user = "destiny2smsupdates@gmail.com"
+    pass_file = open("password.txt", "r")
+    password = pass_file.readline()
+    msg['from'] = user
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+
+    server.quit()
+
+
+def update_checker():
+    pass
+
+
+if __name__ == '__main__':
+    # 1:00 PM
+    # 13:00:00
+    day = 86400
+    check = False
+    while 1:
+        if check is False:
+            now = datetime.now()
+            time.sleep(30)
+            if now.hour == 13 and now.minute == 15 or 16:
+                check = True
+                pull_data()
+                time.sleep(day)
+        else:
+            pull_data()
+            time.sleep(day)
